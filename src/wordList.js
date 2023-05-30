76b76b1d99e39b309999b23d252c5b40481f2e49
vivek chhabra <vivekcb8642@gmail.com>
2023-05-30 11:48:31 +0530

    add wordlist file

---
+++ src/wordList.js
@@ -0,0 +1,25 @@
+const randomWords = [
+    { hint: "Something that you always want to be into the cage .", word: "mouse" },
+    { hint: "Month that has 28 days .", word: "every" },
+    { hint: "What has 13 hearts but no other organs ?", word: "cards" },
+    { hint: "Food source for electronical devices .", word: "charger" },
+    { hint: "I'm tall when I'm young, and short when I'm old .", word: "candle" },
+    { hint: "What can you break, even if you never pick it up or touch it ?", word: "promise" },
+    { hint: "What gets wet while drying ?", word: "towel" },
+    { hint: "How many birthdays you will have when you die at 50 ?", word: "one" },
+    { hint: "What can't talk but will reply when spoken to ?", word: "echo" },
+    { hint: "What is black when its clean and white when its dirty ?", word: "blackboard" },
+    { hint: "What can't talk but will reply when spoken to ? (another)", word: "google" },
+    { hint: "What gets bigger when more is taken away ?", word: "hole" },
+    { hint: "I follow you all the time and copy your every move, but you can’t touch me or catch me .", word: "shadow" },
+    { hint: "It belongs to you, but other people use it more than you do .", word: "myname" },
+    { hint: "What starts with 'P' and ends with 'E' and has more than 1000 letters ?", word: "postoffice" },
+    { hint: "How do you make the number one disappear by adding to it a letter?", word: "gone" },
+    { hint: "What room has no door and window ?", word: "mushroom" },
+    { hint: "What has neck but no head ?", word: "bottle" },
+    { hint: "What is full of holes but still holds water ?", word: "sponge" },
+    { hint: "What goes up but never comes down ?", word: "age" },
+    { hint: "If you’ve got me, you want to share me; if you share me, you haven’t kept me .", word: "secret" },
+];
+
+export default randomWords;
